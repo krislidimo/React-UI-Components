d1e5d650ffce70935a7e1c9d6fa94ee146189b65
Krisli Dimo <krislidimo@gmail.com>
2019-04-23 17:02:03 -0500

    finished NumberButton, ActionButton, and CalculatorDisplay usin reactjs and css

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
+import ActionButton from './components/ButtonComponents/ActionButton'
+import NumberButton from './components/ButtonComponents/NumberButton'
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="App">
+      <CalculatorDisplay />
+      <div className="buttons">
+        <div>
+          <ActionButton value="clear"/>
+          <div className="number-buttons">
+            <NumberButton value="9" buttonStyle='number-button' />
+            <NumberButton value="8" buttonStyle='number-button' />
+            <NumberButton value="7" buttonStyle='number-button' />
+            <NumberButton value="6" buttonStyle='number-button' />
+            <NumberButton value="5" buttonStyle='number-button' />
+            <NumberButton value="4" buttonStyle='number-button' />
+            <NumberButton value="3" buttonStyle='number-button' />
+            <NumberButton value="2" buttonStyle='number-button' />
+            <NumberButton value="1" buttonStyle='number-button' />
+            <ActionButton value="0" buttonStyle='number-button' />
+          </div>
+        </div>
+        <div className="arithmatic-buttons">
+          <NumberButton value="/" buttonStyle="arithmatic-button" />
+          <NumberButton value="*" buttonStyle="arithmatic-button" />
+          <NumberButton value="-" buttonStyle="arithmatic-button" />
+          <NumberButton value="+" buttonStyle="arithmatic-button" />
+          <NumberButton value="=" buttonStyle="arithmatic-button" />
+        </div>
+      </div>
     </div>
   );
 };
